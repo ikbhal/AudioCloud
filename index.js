f872bbdfad274d3717cfc945f47f57400e3caf1e
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2015-01-17 19:18:07 +0000

    changes

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use(session({
 app.use(busboy()); 
 //...
 app.post('/api/v1/audio', function(req, res) {
+    console.log("Handling POST /api/v1/audio");
     var fstream;
     req.pipe(req.busboy);
     req.busboy.on('file', function (fieldname, file, filename) {
@@ -28,14 +29,39 @@ app.post('/api/v1/audio', function(req, res) {
         var fileno = Date.now(); 
         console.log("fileno:" + fileno);
         var newfilename = fileno+'.mp3';
+        console.log("Saving audio file ");
         fstream = fs.createWriteStream(__dirname + '/uploads/' + newfilename);
         file.pipe(fstream);
         fstream.on('close', function () {
-            res.send(fileno);
+            res.end(fileno+'');
         });
     });
 });
 
+app.get('/api/v1/audio/:audioId', function(req, res){
+   console.log("Inside Get /api/v1/audio/:audioId");
+   var audioId = req.params.audioId;
+   console.log("Audio ID:" + audioId);
+   var filePath = path.join(__dirname, '/uploads/'+audioId+'.mp3');
+   var stat = fs.statSync(filePath);
+
+   res.writeHead(200, {
+    'Content-Type': 'audio/mpeg',
+    'Content-Length': stat.size
+   });
+
+   var readStream = fs.createReadStream(filePath);
+   // We replaced all the event handlers with a simple call to readStream.pipe()
+   console.log("Sending audio file");
+   readStream.pipe(res);
+});
+
+app.get('/api/v1/numbertext', function(req, res){
+  console.log("Handling /api/v1/numbertext");
+  var nt = req.query.nt;
+  console.log('nt:'+nt);
+  res.end(nt+'');
+});
 app.get('/', function (req, res) {
   res.send('Audio Cloud');
 });
